18fac67d6125d7446092e0ad49a1ac56505586e6
Nishanth Sakthivel <nishanthsakthivel98@gmail.com>
2024-02-04 11:24:39 +0530

    completed

--- server/server.js
+++ server/server.js
@@ -33,9 +33,7 @@ exports = {
       "folder_id": articleFolderIdFreshservice,
 
     };
-
     console.log("Data: ", data);
-
 
     try {
       const articleCreateResponce = await $request.invokeTemplate(
@@ -45,16 +43,14 @@ exports = {
 
       const response = articleCreateResponce.response;
       console.log("RESPONSE: ", response);
-      
+
       const parseResponse = JSON.parse(response);
-      console.log("Parse: ",parseResponse);
+      console.log("Parse: ", parseResponse);
 
       const gotArticleId = parseResponse.article.id;
-      console.log("GOT: ",gotArticleId);
+      console.log("GOT: ", gotArticleId);
 
-      
-
-      const savedArticleId = $db.set(`${articleId}`, { "articleId": gotArticleId });
+      const savedArticleId = await $db.set(`${articleId}`, { "articleId": `${gotArticleId}` });
       console.log("Saved Fresh service Article ID: ", savedArticleId);
 
     } catch (err) {
@@ -62,13 +58,74 @@ exports = {
     }
 
   },
-  onArticleUpdateCallback: function (payload) {
+  onArticleUpdateCallback: async function (payload) {
     console.log("Logging arguments from onArticleUpdate event: " + JSON.stringify(payload));
 
+    const updatedArticleId = payload.data.article.id;
+    console.log("Updated Article id: ", updatedArticleId);
+
+    const updatedArticleTitle = payload.data.article.title;
+    console.log("Update article title: ", updatedArticleTitle);
+
+    const updatedArticleDescription = payload.data.article.description_text;
+    console.log("Updated article Description: ", updatedArticleDescription);
+
+    let data;
+    let idForUrl;
+    try {
+      const savedarticleId = await $db.get(`${updatedArticleId}`);
+      console.log("Saved Article Id Db: ", savedarticleId);
+
+      idForUrl = savedarticleId.articleId;
+      console.log("only article ID: ", idForUrl);
+
+      data = {
+        "title": `${updatedArticleTitle}`,
+        "description": `${updatedArticleDescription}`
+      };
+      console.log("Updated Data: ", data);
+    } catch (err) {
+      console.log("Error in retriving from db: ", err);
+    }
+
+    try {
+      const resultOfUpdate = await $request.invokeTemplate(
+        "updateArticleFreshService",
+        {
+          context: { idForUrl },
+          body: JSON.stringify(data)
+        }
+      );
+      console.log("Result of update: ", resultOfUpdate);
+
+    } catch (err) {
+      console.log("Error in update article :", err);
+    }
 
   },
-  onArticleDeleteCallback: function (payload) {
+  onArticleDeleteCallback: async function (payload) {
     console.log("Logging arguments from onArticleDelete event: " + JSON.stringify(payload));
+
+    const updatedArticleId = payload.data.article.id;
+    console.log("Updated Article id: ", updatedArticleId); 
+
+    let idForUrl;
+    
+    try {
+      const savedarticleId = await $db.get(`${updatedArticleId}`);
+      console.log("Saved Article Id Db: ", savedarticleId);
+
+      idForUrl = savedarticleId.articleId;
+      console.log("only article ID: ", idForUrl);
+
+      const resultOfDelete = await $request.invokeTemplate(
+        "deletingArticleFreshService", { context: { idForUrl } }
+      );
+      console.log("Result Sucess: ", resultOfDelete);
+
+    } catch (err) {
+      console.log("Error: ", err);
+    }
 
   }
 }
